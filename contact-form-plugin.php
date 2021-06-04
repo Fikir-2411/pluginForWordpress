dde6c5f6240b84ac387a9cab4ddbbf996cb626e4
Fikir <fikirmuluken@gmail.com>
2021-06-04 16:17:52 +0300

    finish the add ons

--- contact-form-plugin.php
+++ contact-form-plugin.php
@@ -1,17 +1,46 @@
 <?php 
 /**
- * Plugin Name: Form for RAD
+ * Plugin Name:       Contact Form for RAD
+ * Description:       A simple contact form plugin that is used for displaying a contact form 
+ * Version:           1.0
+ * Author:            Fikir Muluken 
+ * Author URI:        https://fikirm.wordpress.com
+ * Text Domain:       my-basics-plugin
  */
 
- function rd_form_plugin(){
+ defined('ABSPATH') or die("You missed it");
 
-    $content = '';
-    $content .= 'Hello World!';
+ function rd_contact_plugin(){
+    include_once plugin_dir_path(__FILE__) .'includes/the-form.php';
 
-
-    return $content;
  }
- add_shortcode('rd_form','rd_form_plugin')
+
+ add_shortcode('rd_form','rd_contact_plugin');
+
+
+ function rd_addMenu()
+{
+
+    add_menu_page(
+        'Contact Form',
+        'Contact Form',
+        'manage_options',
+        'contact-form',
+        'displayCode',
+        'dashicons-feedback'
+    );
+
+}
+
+add_action('admin_menu','rd_addMenu');
+
+
+function rd_loadAssets(){
+   wp_enqueue_style('myCSS', plugin_dir_url(__FILE__) .'assets/css/style.css');
+   wp_enqueue_script( 'myScript', plugin_dir_url( __FILE__ ) . 'assets/js/script.js' );
+}
+
+add_action('wp_enqueue_scripts','rd_loadAssets');
 
 
 ?>
